fa41cac050b02072446f4a9d1dcee225038c7fad
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-11-29 22:57:57 +0100

    feature: add set cookies to login

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import express from "express"
 import { MikroORM } from "@mikro-orm/core"
 import { ApolloServer } from "apollo-server-express"
 import { buildSchema } from "type-graphql"
+import redis from "redis"
+import session from "express-session"
+import connectRedis from "connect-redis"
 
 // Config files
 import mikroConfig from "./mikro-orm.config"
@@ -11,6 +14,11 @@ import mikroConfig from "./mikro-orm.config"
 // Resolvers
 import { PostResolver } from "./resolvers/post"
 import { UserResolver } from "./resolvers/user"
+
+// constants
+import { PRODUCTION } from "./constants"
+import { MyContext } from "./types"
+const TEN_YEARS = 1000 * 60 * 60 * 24 * 365 * 10
 
 const main = async () => {
 	// Initialize
@@ -20,12 +28,36 @@ const main = async () => {
 
 	const app = express()
 
+	// Setup redis
+	const RedisStore = connectRedis(session)
+	const redisClient = redis.createClient()
+
+	app.use(
+		session({
+			name: "qid",
+			store: new RedisStore({
+				client: redisClient,
+				disableTouch: true,
+				// disableTTL: true,
+			}),
+			cookie: {
+				maxAge: TEN_YEARS,
+				httpOnly: true,
+				sameSite: "lax", // crsf
+				secure: PRODUCTION, // Cookie only works in https during production
+			},
+			saveUninitialized: false,
+			secret: "keyboard cat", // TODO: Change to environment variables
+			resave: false,
+		})
+	)
+
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
 			resolvers: [PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: () => ({ em: orm.em }),
+		context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
 	})
 
 	apolloServer.applyMiddleware({ app })
@@ -34,3 +66,8 @@ const main = async () => {
 }
 
 main().catch(err => console.error(err))
+
+// Notes:
+// 1. It's important for the session middleware to be written
+//		before the apollo middleware because it will be
+// 		used inside the apollo middleware
--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,17 @@
 import { EntityManager, IDatabaseDriver, Connection } from "@mikro-orm/core"
+import { Request, Response } from "express"
+import session from "express-session"
+
+// I had to create this myself when the tutorial was working and mine
+// wasn't. It could be wrong or there could be a better way to do it.
+interface SessionData {
+	userId?: number
+}
 
 export type MyContext = {
 	em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>
+	req: Request & {
+		session: session.Session & Partial<session.SessionData> & SessionData
+	}
+	res: Response
 }
