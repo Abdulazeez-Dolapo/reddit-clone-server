0a631c93208906ee16aa43ae1b5c2bf63cc5f9f5
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2020-11-29 10:17:56 +0100

    feature: add validation to register endpoint

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -41,25 +41,96 @@ class UserResponse {
 	@Field(() => [FieldError], { nullable: true })
 	errors?: FieldError[]
 
-	@Field()
+	@Field(() => User, { nullable: true })
 	user?: User
 }
 
 @Resolver()
 export class UserResolver {
 	// Register user
-	@Mutation(() => User)
+	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
 		@Ctx() { em }: MyContext
-	) {
+	): Promise<UserResponse> {
+		if (options.username.length < 3) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "username must not be less than 3 characters",
+					},
+				],
+			}
+		}
+
+		if (options.password.length < 8) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "password must not be less than 8 characters",
+					},
+				],
+			}
+		}
+
 		const hashedPassword = await argon2.hash(options.password)
 		const user = em.create(User, {
 			username: options.username,
 			password: hashedPassword,
 		})
-		await em.persistAndFlush(user)
 
-		return user
+		try {
+			await em.persistAndFlush(user)
+		} catch (err) {
+			if (err.code === "23505" || err.detail.includes("already taken")) {
+				return {
+					errors: [
+						{
+							field: "username",
+							message: "username has already been taken",
+						},
+					],
+				}
+			}
+		}
+
+		return { user }
+	}
+
+	@Mutation(() => UserResponse)
+	// Login user
+	async login(
+		@Arg("options") options: UsernamePasswordInput,
+		@Ctx() { em }: MyContext
+	): Promise<UserResponse> {
+		const user = await em.findOne(User, { username: options.username })
+		if (!user) {
+			return {
+				errors: [
+					{
+						field: "username",
+						message: "invalid login credentials",
+					},
+				],
+			}
+		}
+
+		const validPassword = await argon2.verify(user.password, options.password)
+		if (!validPassword) {
+			return {
+				errors: [
+					{
+						field: "password",
+						message: "invalid login credentials",
+					},
+				],
+			}
+		}
+
+		return {
+			user,
+		}
 	}
 }
